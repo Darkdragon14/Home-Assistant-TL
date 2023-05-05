ad403bc3cec877ee000b43a75b1da036ccd02b70
Darkdragon14 <s.tristan@hotmail.fr>
2023-05-05 20:26:24 +0200

    feat: add selector to setup a stop

--- custom_components/tl/config_flow.py
+++ custom_components/tl/config_flow.py
@@ -11,9 +11,14 @@ from .const import (
   CONFIG_BUS,
   CONFIG_DIRECTION,
   DATA_SCHEMA_STOP,
+  CONFIG_WAYBACK
 )
 
-from .utils import get_buses
+from .utils import (
+  get_buses,
+  get_directions,
+  get_stops
+)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -24,18 +29,50 @@ class TlBusConfigFlow(ConfigFlow, domain=DOMAIN):
 
   async def async_step_user(self, user_input):
     """Setup based on user config"""
+    buses = await get_buses()
 
-    errors = {}
     if user_input is not None:
-      # Setup our basic sensors
-      if len(errors) < 1:
-        return self.async_create_entry(
-          title=f"Bus Stop {user_input[CONFIG_NAME]}", 
-          data=user_input
+      if CONFIG_BUS in user_input:
+        directions = await get_directions(user_input[CONFIG_BUS])
+        if CONFIG_DIRECTION in user_input and self.context[CONFIG_BUS] == user_input[CONFIG_BUS]:
+          stops = await get_stops(user_input[CONFIG_DIRECTION])
+          if CONFIG_STOP in user_input and self.context[CONFIG_DIRECTION] == user_input[CONFIG_DIRECTION]:
+            for id, direction in enumerate(directions):
+              if direction == user_input[CONFIG_DIRECTION]:
+                user_input[CONFIG_WAYBACK] = id
+            return self.async_create_entry(
+              title=f"Bus Stop {user_input[CONFIG_NAME]}", 
+              data=user_input
+            )
+          self.context[CONFIG_DIRECTION] = user_input[CONFIG_DIRECTION]
+          return self.async_show_form(
+            step_id='user',
+            data_schema=vol.Schema({
+              vol.Required(CONFIG_NAME, default=user_input[CONFIG_NAME]): str,
+              vol.Required(CONFIG_BUS, default=user_input[CONFIG_BUS]): vol.In(buses),
+              vol.Required(CONFIG_DIRECTION, default=user_input[CONFIG_DIRECTION]): vol.In(directions),
+              vol.Required(CONFIG_STOP): vol.In(stops)
+            }),
+            errors={}
+          )
+        self.context[CONFIG_BUS] = user_input[CONFIG_BUS]
+        return self.async_show_form(
+          step_id='user',
+          data_schema=vol.Schema({
+            vol.Required(CONFIG_NAME, default=user_input[CONFIG_NAME]): str,
+            vol.Required(CONFIG_BUS, default=user_input[CONFIG_BUS]): vol.In(buses),
+            vol.Required(CONFIG_DIRECTION): vol.In(directions)
+          }),
+          errors={}
         )
 
     return self.async_show_form(
-      step_id="user", data_schema=DATA_SCHEMA_STOP, errors=errors
+      step_id='user',
+      data_schema=vol.Schema({
+        vol.Required(CONFIG_NAME): str,
+        vol.Required(CONFIG_BUS): vol.In(buses)
+      }),
+      errors={}
     )
 
   @staticmethod
--- custom_components/tl/utils.py
+++ custom_components/tl/utils.py
@@ -5,7 +5,7 @@ import pytz
 from .const import (
   CONFIG_STOP,
   CONFIG_BUS,
-  CONFIG_DIRECTION
+  CONFIG_WAYBACK
 )
 
 from .api_client import TlApiClient
@@ -16,7 +16,7 @@ async def get_next_bus(api_client: TlApiClient, config, now):
   params = {
       'line': config[CONFIG_BUS],
       'stop': config[CONFIG_STOP],
-      'wayback': config[CONFIG_DIRECTION],
+      'wayback': config[CONFIG_WAYBACK],
       'count': 6
   }
   return await api_client.async_get('/departures', params, current_datetime)
@@ -24,7 +24,30 @@ async def get_next_bus(api_client: TlApiClient, config, now):
 async def get_buses():
   api_client = TlApiClient()
   buses = await api_client.async_get('/lines')
-  busesOption = []
+  buses_dict = {}
   for bus in buses:
-    busesOption.append({'id': bus['id'], 'name': f"{bus['name']} - {bus['longName']}"})
-  return busesOption
+    buses_dict[bus['id']] = f"{bus['name']} - {bus['longName']}"
+  return buses_dict
+
+async def get_directions(busId):
+  api_client = TlApiClient()
+  params = {
+    'line': busId
+  }
+  directions = await api_client.async_get('/routes', params)
+  directions_dict = {}
+  for direction in directions:
+    directions_dict[direction['id']] = direction['direction']
+  return directions_dict
+
+
+async def get_stops(directionId):
+  api_client = TlApiClient()
+  params = {
+    'route': directionId
+  }
+  stops = await api_client.async_get('/stops', params)
+  stops_dict = {}
+  for stop in stops:
+    stops_dict[stop['id']] = f"{stop['name']} - {stop['city']}"
+  return stops_dict
